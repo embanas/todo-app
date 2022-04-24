bfb5661dff3681f4e395a83a1293be269bff2474
embanas <you@example.com>
2022-04-24 12:11:52 +0200

    firebase installed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'todo-app-letsgo',
+    appId: '1:429496705620:web:6135210995385f052e43c1',
+    storageBucket: 'todo-app-letsgo.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCoXTTAyY6mOzOVNX_FXzAkE6Ua55BiAm8',
+    authDomain: 'todo-app-letsgo.firebaseapp.com',
+    messagingSenderId: '429496705620',
+  },
   production: true
 };
